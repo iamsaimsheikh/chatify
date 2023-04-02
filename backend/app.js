eb72beca02a9608b4cc9e3d65430ca472bc2ab61
saim <iamsaimsheikh@gmail.com>
2023-04-02 05:01:51 +0500

    dummy data and apis

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const chats = require("./data/data.js");
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send("working")
-})
+app.get("/", (req, res) => {
+  console.log(chats);
+  res.send(chats);
+});
+
+app.get("/chat/:id", (req, res) => {
+  const chat = chats.find((c) => c._id == req.params.id);
+  res.send(chat);
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
